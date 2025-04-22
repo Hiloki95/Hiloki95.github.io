d730a195c08f9fe770e44c963318bbc876378802
Hiloki95 <131291698+Hiloki95@users.noreply.github.com>
2025-04-22 10:27:44 +0200

    Add files via upload

---
+++ ciao.py
@@ -0,0 +1,77 @@
+import marimo
+
+__generated_with = "0.12.4"
+app = marimo.App(width="full", auto_download=["html"])
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import matplotlib.pyplot as plt
+    import numpy as np
+    return mo, np, plt
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""**ciao** proviamo a fare un grafico stupidissimo""")
+    return
+
+
+@app.cell
+def _(mo):
+    slider_x2 = mo.ui.slider(-5, 5, label=f"Coefficiente x^2: ")
+    slider_x = mo.ui.slider(-5, 5, label=f"Coefficiente x: ")
+    slider_b = mo.ui.slider(-5, 5, label=f"Termine noto: ")
+    return slider_b, slider_x, slider_x2
+
+
+@app.cell
+def _(slider_b, slider_x, slider_x2):
+    slider_x2, slider_x, slider_b
+    return
+
+
+@app.cell
+def _(mo, slider_b, slider_x, slider_x2):
+    a = slider_x2.value
+    b = slider_x.value
+    c = slider_b.value
+    mo.md(rf"""L'equazione mostrata nel grafico è la seguente:  
+    {a}x<sup>2</sup> + {b}x + {c}""")
+    return a, b, c
+
+
+@app.cell
+def _(a, b, c, np, plt):
+    # x = range(-1, 1, 100)
+    x = np.linspace(-10, 10, 1000)
+    y = [a*x**2 + b*x + c for x in x]
+    if a >= 0:
+        lim_inf, lim_sup = -1*max(y), max(y)
+    else:
+        lim_inf, lim_sup = min(y), -1*min(y)
+    asse_x  = np.zeros(len(x))
+    asse_y  = np.zeros(len(x))
+    asse_y2 = np.linspace(lim_inf, lim_sup, len(x))
+    g = plt.plot(x,y)
+    g = plt.plot(asse_y, asse_y2, 'k')
+    g = plt.plot(x, asse_x, 'k')
+    return asse_x, asse_y, asse_y2, g, lim_inf, lim_sup, x, y
+
+
+@app.cell
+def _(g):
+    g
+    return
+
+
+@app.cell
+def _():
+    plt.plot([1,2,3],[4,5,6])
+    mo.mpl.interactive(plt.gcf())
+    return
+
+
+if __name__ == "__main__":
+    app.run()
